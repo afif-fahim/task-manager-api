ef588c0dab12aeee1a5858f3731e88af43d7259c
Afif Fahim <afif2011fahim@gmail.com>
2021-08-23 04:33:35 +0600

    Added API documentation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
+
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath))
 
 app.use(express.json())
 app.use(userRouter)
